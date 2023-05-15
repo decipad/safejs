abcf68fe31b650d7d23aaa723a738790903d540b
John Costa <johncosta027@gmail.com>
2023-05-15 15:31:06 +0100

    CHORE: Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 // Load Web Workers
 import WorkerFile from "./worker.ts?worker";
 
+/** SafeJs is a way to run safe user-provided JavaScript code in a web worker.
+ * The web worker has no access to DOM or window object, see [MDN Docs](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API), and we go even further by re-writing the `get` method of many web worker function to throw error, making them unusable.
+ * We also restrict the `prototype` of the `self` object in the web worker, making the execution of the code safe.
+ * There is also a whitelist which allows the web worker to define what functions are allowed (for example decipads usecase needs `fetch`, but you might not want to allow this).
+ */
 export class SafeJs {
   private worker: Worker;
   private channel: MessageChannel;
